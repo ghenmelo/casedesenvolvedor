90de6ebdfbb20de13c8ea5fa273fde2b19231c22
Guilherm Melo <devilnero2011@gmail.com>
2020-01-22 15:31:06 -0300

    Criando repositorio, com um projeto Sprin-boot gerado por https://start.spring.io/

---
+++ src/main/java/com/casedesenvolvedor/casedesenvolvedor/CasedesenvolvedorApplication.java
@@ -0,0 +1,13 @@
+package com.casedesenvolvedor.casedesenvolvedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasedesenvolvedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasedesenvolvedorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casedesenvolvedor/casedesenvolvedor/CasedesenvolvedorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casedesenvolvedor.casedesenvolvedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasedesenvolvedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
