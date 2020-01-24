9952dfe52b04b51d4724784d83fc1138de3b50ea
Guilherm Melo <devilnero2011@gmail.com>
2020-01-24 18:58:42 -0300

    Alterando cardRepository

--- src/main/java/com/casedesenvolvedor/casedesenvolvedor/Repository/CardRepository.java
+++ src/main/java/com/casedesenvolvedor/casedesenvolvedor/Repository/CardRepository.java
@@ -1,10 +1,15 @@
 package com.casedesenvolvedor.casedesenvolvedor.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.casedesenvolvedor.casedesenvolvedor.model.Card;
 @Repository
-public interface CardRepository extends CrudRepository<Card, Long> {
+public interface CardRepository extends JpaRepository<Card, Long> {
+		
 	
+	@Query("SELECT cardPrimitivo FROM Card c WHERE c.codigo >= 0") List<String> findAllCardsPrimitives();
 }
