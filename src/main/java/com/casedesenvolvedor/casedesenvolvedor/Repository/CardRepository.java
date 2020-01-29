0f7d053a6a4c3b262c02c2dc580304f1399f5403
Guilherm Melo <devilnero2011@gmail.com>
2020-01-28 21:33:51 -0300

    atualizando cardRepository

--- src/main/java/com/casedesenvolvedor/casedesenvolvedor/Repository/CardRepository.java
+++ src/main/java/com/casedesenvolvedor/casedesenvolvedor/Repository/CardRepository.java
@@ -1,5 +1,7 @@
 package com.casedesenvolvedor.casedesenvolvedor.repository;
 
+
+//	
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +11,5 @@ import org.springframework.stereotype.Repository;
 import com.casedesenvolvedor.casedesenvolvedor.model.Card;
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
-		
-	
 	@Query("SELECT cardPrimitivo FROM Card c WHERE c.codigo >= 0") List<String> findAllCardsPrimitives();
 }
